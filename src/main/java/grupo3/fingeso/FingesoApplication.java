980c48c316b60c86caf65ab056140ef725d201db
joaquin <joaquin.pino@usach.cl>
2026-08-11 19:33:01 -0400

    Initial commit

---
+++ src/main/java/grupo3/fingeso/FingesoApplication.java
@@ -0,0 +1,13 @@
+package grupo3.fingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingesoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo3/fingeso/FingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo3.fingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
